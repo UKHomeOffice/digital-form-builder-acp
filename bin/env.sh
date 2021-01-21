2e10fe200cb7d87c430580a48b6ccefe6383d661
Gautam <gautam.pachnanda@digital.homeoffice.gov.uk>
2021-01-21 15:53:42 +0000

    Read tag from .env

--- bin/env.sh
+++ bin/env.sh
@@ -3,12 +3,10 @@
 
 source bin/util.sh || exit 1
 source .env || exit 1
-env
 
 export KUBE_CERTIFICATE_AUTHORITY=https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${DRONE_DEPLOY_TO=notset}.crt
 export ACP_IMAGE_VERSION=${ACP_IMAGE_VERSION:-"latest"}
-
-export BASE_IMAGE_VERSION=${BASE_IMAGE_VERSION:-"latest"}
+export BASE_IMAGE_VERSION=${XGOV_TAG:-"latest"}
 info "---"
 info "ACP_IMAGE_VERSION: ${ACP_IMAGE_VERSION}"
 info "BASE_IMAGE_VERSION: ${BASE_IMAGE_VERSION}"

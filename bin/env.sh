5126ef806ab1d9dcb2460654cb0df9bfa4f6be06
Gautam <gautam.pachnanda@digital.homeoffice.gov.uk>
2021-01-21 16:13:26 +0000

    Print current sha from env

--- bin/env.sh
+++ bin/env.sh
@@ -7,7 +7,9 @@ source .env || exit 1
 export KUBE_CERTIFICATE_AUTHORITY=https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${DRONE_DEPLOY_TO=notset}.crt
 export ACP_IMAGE_VERSION=${ACP_IMAGE_VERSION:-"latest"}
 export BASE_IMAGE_VERSION=${XGOV_TAG:-"latest"}
+export BASE_IMAGE_APP_SHA=${XGOV_SHA:-"master"}
 info "---"
 info "ACP_IMAGE_VERSION: ${ACP_IMAGE_VERSION}"
 info "BASE_IMAGE_VERSION: ${BASE_IMAGE_VERSION}"
+info "BASE_IMAGE_APP_SHA: ${BASE_IMAGE_APP_SHA}"
 info "---"

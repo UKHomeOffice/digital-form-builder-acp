2ac237fc27170b7529edda7eda9e1885aba57f9d
Gautam <gautam.pachnanda@digital.homeoffice.gov.uk>
2021-01-21 15:41:31 +0000

    Rework sonar

--- bin/env.sh
+++ bin/env.sh
@@ -3,6 +3,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/util.sh || exit 1
+source .env || exit 1
 
 export KUBE_CERTIFICATE_AUTHORITY=https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${DRONE_DEPLOY_TO=notset}.crt
 export ACP_IMAGE_VERSION=${ACP_IMAGE_VERSION:-"latest"}

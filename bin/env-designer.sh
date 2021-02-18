cca8e5603f26f0c77c3ebeac0a50da9271e0d1cf
Gautam Pachnanda <gautam.pachnanda@digital.homeoffice.gov.uk>
2021-02-18 17:42:06 +0000

    Fix shell scripts

--- bin/env-designer.sh
+++ bin/env-designer.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 [[ -z "${DEBUG}" ]] || set -x
 source .env
+source bin/util.sh
 export BASE_IMAGE_TAG="$XGOV_TAG"
 echo "$BASE_IMAGE_TAG"
 eval "echo \"$(cat designer/Dockerfile)\"" > designer/Dockerfile.out
+info "Docker file processed"
 
 
--- bin/env-runner.sh
+++ bin/env-runner.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 [[ -z "${DEBUG}" ]] || set -x
 source .env
+source bin/util.sh
 export BASE_IMAGE_TAG="$XGOV_TAG"
 echo "$BASE_IMAGE_TAG"
 eval "echo \"$(cat runner/Dockerfile)\"" > runner/Dockerfile.out
+info "Docker file processed"

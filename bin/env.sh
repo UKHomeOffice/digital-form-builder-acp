2f1f4f68b6a163f10b9e23fbd78d4d22e8d999ff
Gautam Pachnanda <gautam.pachnanda@digital.homeoffice.gov.uk>
2021-02-18 16:50:02 +0000

    New implemenation for replacing base tag

--- bin/env.sh
+++ bin/env.sh
@@ -2,7 +2,7 @@
 [[ -z "${DEBUG}" ]] || set -x
 source .env
 export BASE_IMAGE_TAG="$XGOV_TAG"
-echo ""$BASE_IMAGE_TAG"
+echo "$BASE_IMAGE_TAG"
 eval "echo \"$(cat designer/Dockerfile)\"" > designer/Dockerfile.out
 cat designer/Dockerfile.out
 

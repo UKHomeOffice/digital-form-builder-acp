71089c2ffcf667b09c505fb9a1e76f3446de9553
Gautam Pachnanda <gautam.pachnanda@digital.homeoffice.gov.uk>
2021-02-23 18:03:55 +0000

    Adding debug for deploy variables

--- bin/designer.sh
+++ bin/designer.sh
@@ -6,7 +6,9 @@ export BASE_IMAGE_TAG="$XGOV_TAG"
 echo "$BASE_IMAGE_TAG"
 eval "echo \"$(cat designer/Dockerfile)\"" > designer/Dockerfile.out
 echo -n "$XGOV_TAG,latest,$DRONE_COMMIT_SHA" > .tags
-echo "DESIGNER_TAG=$XGOV_TAG" > .designer
+echo "DESIGNER_TAG=$XGOV_TAG" > /drone/src/.designer
+pwd
+ls -lah
 info "Docker file processed"
 
 
--- bin/runner.sh
+++ bin/runner.sh
@@ -6,5 +6,7 @@ export BASE_IMAGE_TAG="$XGOV_TAG"
 echo "$BASE_IMAGE_TAG"
 eval "echo \"$(cat runner/Dockerfile)\"" > runner/Dockerfile.out
 echo -n "$XGOV_TAG,latest,$DRONE_COMMIT_SHA" > .tags
-echo "RUNNER_TAG=$XGOV_TAG" > .runner
+echo "RUNNER_TAG=$XGOV_TAG" > /drone/src/.runner
+pwd
+ls -lah
 info "Docker file processed"

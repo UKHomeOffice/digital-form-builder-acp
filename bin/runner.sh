e106918a1f4c8aad75fc824dbd6ba98adf8bb635
Enda McCormack <endamccormack0@gmail.com>
2021-03-18 08:27:08 +0000

    updated docker templater to use git tag from drone_tag

--- bin/runner.sh
+++ bin/runner.sh
@@ -2,11 +2,9 @@
 [[ -z "${DEBUG}" ]] || set -x
 source .env
 source bin/util.sh
-export BASE_IMAGE_TAG="$XGOV_TAG"
-echo "$BASE_IMAGE_TAG"
 sed 's/$BASE_IMAGE_TAG/'"$XGOV_TAG"'/' runner/Dockerfile > runner/Dockerfile.out
-echo -n "$XGOV_TAG,latest,$DRONE_COMMIT_SHA" > .tags
-echo "RUNNER_TAG=$XGOV_TAG" > .runner
+echo -n "$DRONE_TAG,latest,$DRONE_COMMIT_SHA" > .tags
+echo "RUNNER_TAG=$DRONE_TAG" > .runner
 echo
 cat .runner
 pwd
@@ -16,4 +14,3 @@ echo
 echo "Reading docker file for runner"
 cat designer/Dockerfile.out
 info "Docker file processed for runner"
-

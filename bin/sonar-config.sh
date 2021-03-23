7d660556867758bac3b26648d73abdcbfd6abfbf
Gautam Pachnanda <gautam.pachnanda@digital.homeoffice.gov.uk>
2021-03-23 18:29:34 +0000

    Added debug for token configuration

--- bin/sonar-config.sh
+++ bin/sonar-config.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 [[ -z "${DEBUG}" ]] || set -x
+source bin/util.sh
 if [[ -z "${SONAR_TOKEN}" ]]; then
-  echo "Missing Sonar Token"
+  error "Missing Sonar Token"
   exit 1
 else
-  "Found sonar token"
+  info "Found sonar token"
 fi
 sed 's/$USER/'"${SONAR_TOKEN}"'/' digital-form-builder/sonar-project.properties > digital-form-builder/sonar-project.properties
-cat digital-form-builder/sonar-project.properties
+info "Token configured OK"
